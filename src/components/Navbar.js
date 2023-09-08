0ad32d65fd121ece1191cdfd008b171b2de73689
faresh9 <93577791+faresh9@users.noreply.github.com>
2023-09-08 22:23:56 +0300

    .

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,20 @@
-import horoscopesObj from '../data/horoscopes';
-
+import horoscopesObj from "../data/horoscopes";
+import { useContext } from "react";
+import { HoroscopeContext } from "../context/HoroscopeContext";
 const Navbar = () => {
+  console.log("Navbar is SR ");
   const horoscopes = Object.keys(horoscopesObj);
+  const { setCurrentSign } = useContext(HoroscopeContext);
 
   return (
     <nav>
-      {horoscopes.map(sign => (
-        <span key={sign}>
-        {sign}
+      {horoscopes.map((sign) => (
+        <span key={sign} onClick={(e) => setCurrentSign(sign)}>
+          {sign}
         </span>
       ))}
     </nav>
-  )
+  );
 };
 
 export default Navbar;
